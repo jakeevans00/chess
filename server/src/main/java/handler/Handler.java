9c8fcb9faf20e12b700eb0a19797c01e7268ba4d
jakeevans00 <ejake370@gmail.com>
2024-07-15 16:05:04 -0600

    Create Service, Handler, and Data Model classes

---
+++ server/src/main/java/handler/Handler.java
@@ -0,0 +1,11 @@
+package handler;
+
+import spark.Request;
+import spark.Response;
+
+
+public class Handler {
+    public Object handleLogin(Request request, Response response) {
+        return new Object();
+    }
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import handler.Handler;
 import spark.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Server {
 
@@ -10,9 +14,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        createRoutes();
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -21,5 +23,16 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    public static void createRoutes() {
+        Spark.get("/", ((request, response) -> {
+            response.type("text/html");
+            return new String(Files.readAllBytes(Paths.get("src/main/resources/public/index.html")));
+        } ));
+
+        Spark.post("/session", ((request, response) -> {
+            return new Handler().handleLogin(request, response);
+        }));
     }
 }
