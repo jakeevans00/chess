efb288fb3c3d0a10c238c25273c0b5344f9de6f4
jakeevans00 <ejake370@gmail.com>
2024-07-30 14:19:20 -0600

    Implement hashing for user secrets

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -3,6 +3,7 @@ package service;
 import dataaccess.*;
 import model.AuthData;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 import response.LoginResponse;
 import response.LogoutResponse;
 import response.RegisterResponse;
@@ -23,10 +24,11 @@ public class UserService {
 
     public RegisterResponse register(UserData user) throws Exception {
         validateUser(user);
+        UserData newUser = hashUserData(user);
 
         return ServiceUtils.execute(() -> {
-            userDAO.createUser(user);
-            AuthData auth = authDAO.createAuth(new AuthData(user.username(), UUID.randomUUID().toString()));
+            userDAO.createUser(newUser);
+            AuthData auth = authDAO.createAuth(new AuthData(newUser.username(), UUID.randomUUID().toString()));
             return new RegisterResponse(auth.username(), auth.authToken());
         });
     }
@@ -34,7 +36,7 @@ public class UserService {
     public LoginResponse login(UserData user) throws Exception {
         UserData result = userDAO.getUser(user.username());
 
-        if (result == null || !user.password().equals(result.password())) {
+        if (result == null || !BCrypt.checkpw(user.password(), result.password())) {
             throw new InvalidCredentialsException("Error: Unauthorized");
         }
 
@@ -62,5 +64,10 @@ public class UserService {
         if (result != null) {
             throw new ExistingUserException("Error: Username already exists");
         }
+    }
+
+    private UserData hashUserData(UserData user) {
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        return new UserData(user.username(), hashedPassword, user.email());
     }
 }
