d4e8bf25a618c15950670f47b0ad57107c7dfa1a
jakeevans00 <ejake370@gmail.com>
2024-08-02 19:07:22 -0600

    Create options object

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,7 +1,16 @@
+import server.Server;
 import ui.ConsoleApp;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Server server = new Server();
+            server.run(8080);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
         ConsoleApp consoleManager = new ConsoleApp();
         consoleManager.start();
     }
--- client/src/main/java/ui/ConsoleApp.java
+++ client/src/main/java/ui/ConsoleApp.java
@@ -1,33 +1,36 @@
 package ui;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 import ui.EscapeSequences;
+
+import static javax.swing.UIManager.put;
 
 public class ConsoleApp {
     Scanner scanner = new Scanner(System.in);
     ServerFacade serverFacade = new ServerFacade();
+    boolean loggedIn = false;
 
     public void start() {
-        boolean loggedIn = false;
         boolean run = true;
 
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY);
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY);
         System.out.printf("\uD83D\uDD25\uD83D\uDD25 Let's play some chess! " +
                 "Type 'help' to get started \uD83D\uDD25\uD83D\uDD25%n" +
                 "[LOGGED OUT] >>> ");
 
         while (run) {
-            System.out.println(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
             String command = scanner.nextLine();
             String[] commandArgs = command.split(" ");
 
             switch (commandArgs[0]) {
                 case "help":
-                    System.out.println("some other things");
+                    displayOptions();
                     break;
                 case "login":
-                    loggedIn = true;
-                    System.out.println("Now logged in");
+                    serverFacade.login(commandArgs[1], commandArgs[2]);
                     break;
                 case "end":
                     System.out.println("end");
@@ -38,11 +41,29 @@ public class ConsoleApp {
                     System.out.println("Invalid command");
             }
 
-            if (loggedIn) {
-                System.out.printf("%n[LOGGED IN] >> ");
-            } else {
-                System.out.printf("%n[LOGGED OUT] >> ");
-            }
+            showUser();
         }
     }
+
+    private void showUser() {
+        if (loggedIn) {
+            System.out.printf(EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY + "%n[LOGGED IN]");
+        } else {
+            System.out.printf(EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY + "%n[LOGGED OUT]");
+        }
+    }
+
+    private void displayOptions() {
+        if (loggedIn) {
+            System.out.printf("%n[LOGGED IN] >> ");
+        } else {
+            UNAUTHENTICATED_COMMANDS.forEach((cmd, desc) ->
+                    System.out.printf("\t" + EscapeSequences.SET_TEXT_COLOR_BLUE + cmd + EscapeSequences.SET_TEXT_COLOR_MAGENTA + desc + "\n"));
+        }
+    }
+
+    private static final Map<String, String> UNAUTHENTICATED_COMMANDS = new LinkedHashMap<String, String>() {{
+        put("register <USERNAME> <PASSWORD> <EMAIL>", " - to create an account");
+        put("login <USERNAME> <PASSWORD>", " - to login");
+    }};
 }
